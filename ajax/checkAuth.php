83753db9aa8eaefaa3711d7f2277d6638fb6bb0f
Dusan Klinec <dusan.klinec@gmail.com>
2017-04-28 22:19:08 +0200

    checkAuth - do always the vpn check
    - to know whether auth is possible.

--- ajax/checkAuth.php
+++ ajax/checkAuth.php
@@ -8,24 +8,26 @@ $config = \OC::$server->getConfig();
 $app = new OCA\User_VPNAuth\AppInfo\Application();
 
 $currentUser = false;
-if (OCP\User::isLoggedIn()) {
-    $currentUser = \OC::$server->getUserSession()->getUser();
-    echo json_encode(array(
-        'state' => 'auth',
-        'user' => array(
-            'uid' => $currentUser->getUID(),
-            'name' => $currentUser->getDisplayName(),
-            'email' => $currentUser->getEMailAddress()
-        )
-    ));
-    exit();
-
-}
 
 // User is not already logged in - check out vpn auth state
 $vpnAuth = $app->resolveAuthBackend();
 $authState = $vpnAuth->checkVpnAuth();
+
 if ($authState === false){
+    if (OCP\User::isLoggedIn()) {
+        $currentUser = \OC::$server->getUserSession()->getUser();
+        echo json_encode(array(
+            'state' => 'auth',
+            'user' => array(
+                'uid' => $currentUser->getUID(),
+                'name' => $currentUser->getDisplayName(),
+                'email' => $currentUser->getEMailAddress()
+            )
+        ));
+        exit();
+
+    }
+
     echo json_encode(array(
         'state' => 'unauth'
     ));
